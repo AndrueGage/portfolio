66f460175a7fca81ad80d3c7a76db0a63a5f69ee
AndrueGage <trainer.andrue@gmail.com>
2024-06-13 18:38:23 -0500

    added theme provider and homepage. Imported necessary dependencies and ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import {DarkModeToggle} from "@/components/ui/darkmode-toggle"
+
 
 export default function Home() {
   return (
-   <main className="">
-    <h1>Hello Andrue</h1>
+   <main className="p-10">
+    <div className="flex justify-between">
+    <h1 className="font-black text-3xl">Andrue Desmarais</h1>
+    <DarkModeToggle />
+    </div>
+    <div>
+    <h2 className="font-black text-2xl">A Full Stack Developer</h2>
+    </div>
    </main>
   );
 }
