3ffd01d6257e61d4f22615a087633b092390796f
AndrueGage <trainer.andrue@gmail.com>
2024-06-13 19:04:38 -0500

    created links in navbar component and added navbar to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import {DarkModeToggle} from "@/components/ui/darkmode-toggle"
+import { DarkModeToggle } from "@/components/ui/darkmode-toggle"
 
 
 export default function Home() {
   return (
-   <main className="p-10">
-    <div className="flex justify-between">
-    <h1 className="font-black text-3xl">Andrue Desmarais</h1>
-    <DarkModeToggle />
-    </div>
-    <div>
-    <h2 className="font-black text-2xl">A Full Stack Developer</h2>
-    </div>
-   </main>
+    <main className="p-10">
+      <section>
+        <div className="flex justify-between">
+          <h1 className="text-4xl lg:text-6xl mb-4 font-extrabold">Andrue Desmarais</h1>
+          <DarkModeToggle />
+        </div>
+        <div>
+          <h2 className="font-black text-2xl">A Full Stack Developer</h2>
+        </div>
+      </section>
+    </main>
   );
 }
