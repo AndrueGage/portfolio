13f5e9cc77cf5f0e8c54418a54972a86ddf2ddf3
AndrueGage <trainer.andrue@gmail.com>
2024-06-17 16:59:20 -0500

    added dark and light mode gradient styling. Resized image

--- src/components/custom/Image.tsx
+++ src/components/custom/Image.tsx
@@ -5,11 +5,11 @@ export default function Picture() {
     <span className='flex justify-end pr-8'>
     <Image
       src="/andrue.png"
-      width={250}
-      height={250}
-      sizes="(max-width: 768px) 100vw, 33vw"
+      width={400}
+      height={400}
+      sizes="(max-width: 800px) 100vw, 33vw"
       alt="A picture of me and my dog"
-      className='border-solid border-black border-2 rounded-md object-cover'
+      className='border-solid border-black border-2 rounded-xl object-cover'
     />
     </span>
   )
