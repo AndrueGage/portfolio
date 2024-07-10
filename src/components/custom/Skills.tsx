ac98f46bb2436986c7c3f2640acce24d28a5e980
AndrueGage <trainer.andrue@gmail.com>
2024-07-10 00:17:57 -0500

    added svgs, imported svgs to skill page, started adding svgs using next/image

--- src/components/custom/Skills.tsx
+++ src/components/custom/Skills.tsx
@@ -1,8 +1,32 @@
+import Image from "next/image"
+import Angular from "../../../public/Angular.svg" 
+import JavaScript from "../../../public/JavaScript.svg"
+import MongoDB from "../../../public/MongoDB.svg"
+import MySql from "../../../public/MySQL.svg"
+import NextJS from "../../../public/NextJS.svg"
+import ReactIcon from "../../../public/React.svg"
+import TailwindCss from "../../../public/TailwindCSS.svg"
+import Typescript from "../../../public/Typescript.svg"
+import { any } from "prop-types"
+
 export default function Skills() {
     return (
         <section id="skills">
             <h1 className="text-4xl lg:text-6xl mb-4 font-extrabold">Skills</h1>
-            <p>Skills will go here</p>
+            <div>
+            <Image
+      priority
+      src={JavaScript}
+      alt="JavaScript Icon"
+      width={100}
+            />
+            <Image
+      priority
+      src={NextJS}
+      alt="Nextjs Icon"
+      width={100}
+            />
+            </div>
         </section>
     )
 }
