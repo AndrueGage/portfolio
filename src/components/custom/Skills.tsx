3c7ea124dfb684c645995f5f96508f5c69140bbc
AndrueGage <trainer.andrue@gmail.com>
2024-07-11 23:11:02 -0500

    finished styling the Skills section

--- src/components/custom/Skills.tsx
+++ src/components/custom/Skills.tsx
@@ -9,25 +9,31 @@ import TailwindCss from "../../../public/TailwindCSS.svg"
 import Typescript from "../../../public/Typescript.svg"
 
 const skills = {
-    JavaScript: { src: JavaScript, alt: "JavaScript Icon" },
-    ReactIcon: { src: ReactIcon, alt: "React Icon" },
-    NextJS: { src: NextJS, alt: "NextJS Icon" },
-    TailwindCss: { src: TailwindCss, alt: "TailwindCSS Icon" },
-    MySql: { src: MySql, alt: "MySQL Icon" },
-    MongoDB: { src: MongoDB, alt: "MongoDB Icon" },
-    Typescript: { src: Typescript, alt: "Typescript Icon" },
-    Angular: { src: Angular, alt: "Angular Icon" },
+    JavaScript: { src: JavaScript, alt: "JavaScript Icon", text: "JavaScript"},
+    ReactIcon: { src: ReactIcon, alt: "React Icon", text: "React" },
+    NextJS: { src: NextJS, alt: "NextJS Icon", text: "NextJS" },
+    TailwindCss: { src: TailwindCss, alt: "TailwindCSS Icon", text: "TailwindCSS" },
+    MySql: { src: MySql, alt: "MySQL Icon", text: "MySQL" },
+    MongoDB: { src: MongoDB, alt: "MongoDB Icon", text: "MongoDB"},
+    Typescript: { src: Typescript, alt: "Typescript Icon", text: "TypeScript" },
+    Angular: { src: Angular, alt: "Angular Icon", text: "Angular" },
   };
 
-export default function Skills() {
+  export default function Skills() {
     return (
-        <section id="skills">
-            <h1 className="text-4xl lg:text-6xl mb-4 font-extrabold">Skills</h1>
-            <div className="flex justify-center">
-        {Object.entries(skills).map(([key, { src, alt }]) => (
-          <Image key={key} priority src={src} alt={alt} width={100} />
-        ))}
-      </div>
-        </section>
-    )
-}
+      <section id="skills" className="p-4">
+        <h1 className="text-4xl lg:text-6xl mb-8 font-extrabold text-center">Skills</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {Object.entries(skills).map(([key, { src, alt, text }]) => (
+            <div key={key} className="bg-gradient p-6 rounded-lg shadow-md flex items-center justify-center h-32 object-contain">
+             <div className=" h-16 w-16 relative">
+              <Image priority src={src} alt={alt} layout="fill" objectFit="contain" />
+            </div>
+              <p className="ml-4 text-lg font-semibold">{text}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+  
