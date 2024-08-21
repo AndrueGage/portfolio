094fb709e536ab5dcbe62dc2fddd7d48b2c51edf
AndrueGage <trainer.andrue@gmail.com>
2024-08-21 16:05:21 -0500

    line height change for about me section

--- src/components/custom/About.tsx
+++ src/components/custom/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
         <section id="about">
             <div className=" sm:text-center lg:text-left md:text-left ">
                 <h1 className="text-4xl lg:text-6xl mb-8 font-extrabold">About</h1>
-                <p className="text-xl">  My name is Andrue and I am a full stack developer from Nashville, TN.<br></br>
+                <p className="text-xl leading-10">  My name is Andrue and I am a full stack developer from Nashville, TN.<br></br>
                     I am a purposeful professional with six years of experience as a successful dog trainer, showcasing a proven track record of managing a business with 30+ employees. Leveraging a deep understanding of learning theory, consistently achieved exceptional results in canine behavior modification. Eager to transition into the dynamic field of Information Technology, completing the Full Stack Developer Bootcamp at Vanderbilt University, demonstrating a commitment to enhancing the technical skills required for this industry. Possessing a unique blend of leadership, problem-solving, and adaptability, poised to bring a fresh perspective and a strong work ethic to the IT sector, capitalizing on my diverse background to drive success in new and challenging environments.</p>
             </div>
         </section>
