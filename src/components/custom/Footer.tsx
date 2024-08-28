038c47837b9f94e237843ed35144e9fc9304e25d
AndrueGage <trainer.andrue@gmail.com>
2024-08-28 13:26:53 -0500

    wrapped footer containers in <a>

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -5,23 +5,23 @@ import instagram from "../../../public/instagram.svg"
 
 
 const svgLinks = {
-    Github: { src: github, alt: 'Github Icon', url:'https://github.com/AndrueGage'},
-    LinkedIn: { src: Linkedin, alt: 'LinkedIn Icon', url:'https://www.linkedin.com/in/andrue-desmarais/'},
-    instagram: { src: instagram, alt:'Instagram icon', url:'https://www.instagram.com/androogage/?hl=en'}
+    Github: { src: github, alt: 'Github Icon', url: 'https://github.com/AndrueGage' },
+    LinkedIn: { src: Linkedin, alt: 'LinkedIn Icon', url: 'https://www.linkedin.com/in/andrue-desmarais/' },
+    instagram: { src: instagram, alt: 'Instagram icon', url: 'https://www.instagram.com/androogage/?hl=en' }
 };
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <section className="mt-10">
             <div className="grid grid-cols-1 sm:grid-col-2 lg:grid-cols-3 gap-10">
-                {Object.entries(svgLinks).map(([key, {src, alt, url}]) => (
-                    <div key={key} className="bg-gradient p-6 shadow-md flex items-center justify-center h-14 rounded-lg">
-                        <div className="h-10 w-10 relative">
-                            <a href={url} target='_blank'>
-                            <Image priority src={src} alt={alt} layout="fill" objectFit="contain" className="svg-icon fill-current text-black dark:text-white"/>
-                            </a>
+                {Object.entries(svgLinks).map(([key, { src, alt, url }]) => (
+                    <a href={url} target='_blank'>
+                        <div key={key} className="bg-gradient p-6 shadow-md flex items-center justify-center h-14 rounded-lg">
+                            <div className="h-10 w-10 relative">
+                                <Image priority src={src} alt={alt} layout="fill" objectFit="contain" className="svg-icon fill-current text-black dark:text-white" />
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 ))}
             </div>
         </section>
