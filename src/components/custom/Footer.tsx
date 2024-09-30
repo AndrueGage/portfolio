b274e2b4fbbb6fb43939f9eafb5d2ec158f758ee
AndrueGage <trainer.andrue@gmail.com>
2024-09-30 14:28:47 -0500

    added resume icon and path to resume in About card

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -2,19 +2,21 @@ import Image from "next/image"
 import github from "/public/github.svg"
 import linkedin from "/public/linkedin.svg"
 import instagram from "/public/instagram.svg"
+import resume from "/public/Resume.svg"
 
 const svgLinks = {
     Github: { src: github, alt: 'Github Icon', url: 'https://github.com/AndrueGage' },
     LinkedIn: { src: linkedin, alt: 'LinkedIn Icon', url: 'https://www.linkedin.com/in/andrue-desmarais/' },
-    Instagram: { src: instagram, alt: 'Instagram Icon', url: 'https://www.instagram.com/androogage/?hl=en' }
+    Instagram: { src: instagram, alt: 'Instagram Icon', url: 'https://www.instagram.com/androogage/?hl=en' },
+    Resume: { src: resume, alt: 'Resume Icon', url: '/Andrue_Desmarais_Resume.pdf' }
 };
 
 export default function Footer() {
     return (
-        <div className="bottom-0 right-0 flex gap-2 mt-6 "> 
+        <div className="bottom-0 right-0 flex gap-3 mt-6 ">
             {Object.entries(svgLinks).map(([key, { src, alt, url }]) => (
                 <a href={url} key={key} target="_blank" rel="noopener noreferrer">
-                    <div className="h-6 w-8 relative transform transition-transform ease-in-out duration-300 hover:scale-110">
+                    <div className="h-8 w-8 relative transform transition-transform ease-in-out duration-300 hover:scale-110">
                         <Image priority src={src} alt={alt} layout="fill" objectFit="contain" />
                     </div>
                 </a>
