d0427e146e47b842baf98674c823df3608707fb8
AndrueGage <trainer.andrue@gmail.com>
2024-10-09 12:45:54 -0500

    added tooltip for icons and changed resume icon

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -2,25 +2,44 @@ import Image from "next/image"
 import github from "/public/github.svg"
 import linkedin from "/public/linkedin.svg"
 import instagram from "/public/instagram.svg"
-import resume from "/public/Resume.svg"
+import { PiReadCvLogo } from "react-icons/pi";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
 
 const svgLinks = {
     Github: { src: github, alt: 'Github Icon', url: 'https://github.com/AndrueGage' },
     LinkedIn: { src: linkedin, alt: 'LinkedIn Icon', url: 'https://www.linkedin.com/in/andrue-desmarais/' },
     Instagram: { src: instagram, alt: 'Instagram Icon', url: 'https://www.instagram.com/androogage/?hl=en' },
-    Resume: { src: resume, alt: 'Resume Icon', url: '/Andrue_Desmarais_Resume.pdf' }
+    Resume: { src: null, alt: 'Resume Icon', url: '/Andrue_Desmarais_Resume.pdf' }
 };
 
 export default function Footer() {
     return (
-        <div className="bottom-0 right-0 flex gap-3 mt-6 ">
-            {Object.entries(svgLinks).map(([key, { src, alt, url }]) => (
-                <a href={url} key={key} target="_blank" rel="noopener noreferrer">
-                    <div className="h-8 w-8 relative transform transition-transform ease-in-out duration-300 hover:scale-110">
-                        <Image priority src={src} alt={alt} layout="fill" objectFit="contain" />
-                    </div>
+      <TooltipProvider>
+        <div className="bottom-0 right-0 flex gap-3 mt-6">
+          {Object.entries(svgLinks).map(([key, { src, alt, url }]) => (
+            <Tooltip key={key}>
+              <TooltipTrigger asChild>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  <div className="h-8 w-8 relative transform transition-transform ease-in-out hover:scale-110">
+                    {src ? (
+                      <Image priority src={src} alt={alt} layout="fill" objectFit="contain" />
+                    ) : (
+                      <PiReadCvLogo className="h-8 w-8" />
+                    )}
+                  </div>
                 </a>
-            ))}
+              </TooltipTrigger>
+              <TooltipContent className="rounded-full py-2 ">
+                <p>{key}</p>
+              </TooltipContent>
+            </Tooltip>
+          ))}
         </div>
-    );
-}
+      </TooltipProvider>
+    )
+  }
